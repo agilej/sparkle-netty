226eb058b45403d358e5d1f9432cd6938959e4c2
Donny <donnior@gmail.com>
2014-01-25 09:51:10 +0800

    extract instance variables

--- src/main/java/me/donnior/sparkle/netty/NettySpecific.java
+++ src/main/java/me/donnior/sparkle/netty/NettySpecific.java
@@ -6,14 +6,17 @@ import me.donnior.sparkle.ext.EnvSpecific;
 
 public class NettySpecific implements EnvSpecific {
 
+    private final NettyParamResolversManager nprm = new NettyParamResolversManager();
+    private final ViewRendersResovler vrr = new ViewRendersResovler();
+
     @Override
     public ParamResolversManager getParamsResolverManager() {
-        return new NettyParamResolversManager();
+        return nprm;
     }
 
     @Override
     public ViewRendersResovler getViewRendersResovler() {
-        return new ViewRendersResovler();
+        return vrr;
     }
 
 }
