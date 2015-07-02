b193819dc1c763e497489a6388a3b7dbb6fe48a8
Donny <donnior@gmail.com>
2015-07-02 11:37:48 +0800

    update package

--- src/main/java/me/donnior/sparkle/netty4/NettyParamResolversManager.java
+++ src/main/java/me/donnior/sparkle/netty4/NettyParamResolversManager.java
@@ -1,17 +1,15 @@
 package me.donnior.sparkle.netty4;
 
-import me.donnior.sparkle.core.resolver.AbstractArgumentResolverManager;
-import me.donnior.sparkle.core.resolver.PathVariableArgumentResolver;
-import me.donnior.sparkle.core.resolver.SimpleArgumentResolver;
-import me.donnior.sparkle.core.resolver.WebRequestArgumentResolver;
+import me.donnior.sparkle.core.argument.*;
 
 
 public class NettyParamResolversManager extends AbstractArgumentResolverManager {
     
     public NettyParamResolversManager() {
-        registerArgumentResolver(new WebRequestArgumentResolver());
         registerArgumentResolver(new SimpleArgumentResolver());
+        registerArgumentResolver(new WebRequestArgumentResolver());
         registerArgumentResolver(new PathVariableArgumentResolver());
+        registerArgumentResolver(new ParamsArgumentResolver());
     }
     
 }
