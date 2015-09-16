5879e406e933eb6ea051d4cdc6f6b9dc972aacd7
Donny <donnior@gmail.com>
2015-09-16 18:53:33 +0800

    update for new view renders registration strategy.

--- src/main/java/me/donnior/sparkle/netty4/NettySpecific.java
+++ src/main/java/me/donnior/sparkle/netty4/NettySpecific.java
@@ -4,21 +4,17 @@ import me.donnior.sparkle.core.argument.ArgumentResolverManager;
 import me.donnior.sparkle.core.view.ViewRenderManager;
 import me.donnior.sparkle.engine.RequestLifeCycleManager;
 import me.donnior.sparkle.ext.EnvSpecific;
+import me.donnior.sparkle.ext.VendorViewRenderProvider;
 
 public class NettySpecific implements EnvSpecific {
 
     private final NettyParamResolversManager nprm = new NettyParamResolversManager();
-    private final ViewRenderManager vrr = new ViewRenderManager();
+
     private RequestLifeCycleManager rlcm = new NettyRequestLifeCycleManager();
 
     @Override
     public ArgumentResolverManager getArgumentResolverManager() {
         return nprm;
-    }
-
-    @Override
-    public ViewRenderManager getViewRendersManager() {
-        return vrr;
     }
     
     @Override
@@ -26,4 +22,8 @@ public class NettySpecific implements EnvSpecific {
         return rlcm ;
     }
 
+    @Override
+    public VendorViewRenderProvider vendorViewRenderProvider() {
+        return null;
+    }
 }
