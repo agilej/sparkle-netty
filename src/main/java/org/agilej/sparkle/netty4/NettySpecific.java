386a32a5a6af0660d2aa7c3bcc97115b88a43226
Donny <donnior@gmail.com>
2016-11-15 16:17:50 +0800

    update vendor setting

--- src/main/java/org/agilej/sparkle/netty4/NettySpecific.java
+++ src/main/java/org/agilej/sparkle/netty4/NettySpecific.java
@@ -3,19 +3,13 @@ package org.agilej.sparkle.netty4;
 import org.agilej.sparkle.core.argument.ArgumentResolverManager;
 import org.agilej.sparkle.core.engine.RequestLifeCycleManager;
 import org.agilej.sparkle.core.ext.EnvSpecific;
+import org.agilej.sparkle.core.ext.VendorArgumentResolverProvider;
 import org.agilej.sparkle.core.ext.VendorViewRenderProvider;
 
 public class NettySpecific implements EnvSpecific {
 
-    private final NettyParamResolversManager nprm = new NettyParamResolversManager();
-
     private RequestLifeCycleManager rlcm = new NettyRequestLifeCycleManager();
 
-    @Override
-    public ArgumentResolverManager getArgumentResolverManager() {
-        return nprm;
-    }
-    
     @Override
     public RequestLifeCycleManager getLifeCycleManager() {
         return rlcm ;
@@ -23,6 +17,11 @@ public class NettySpecific implements EnvSpecific {
 
     @Override
     public VendorViewRenderProvider vendorViewRenderProvider() {
+        return null;
+    }
+
+    @Override
+    public VendorArgumentResolverProvider vendorArgumentResolverProvider() {
         return null;
     }
 }
