82a5ea4c3b14edf3ce994d7af001122058412840
Donny <donnior@gmail.com>
2016-11-16 20:27:34 +0800

    update dependency

--- src/main/java/org/agilej/sparkle/netty4/NettyRequestLifeCycleManager.java
+++ src/main/java/org/agilej/sparkle/netty4/NettyRequestLifeCycleManager.java
@@ -1,7 +1,8 @@
 package org.agilej.sparkle.netty4;
 
-import org.agilej.sparkle.core.action.ActionMethod;
-import org.agilej.sparkle.core.engine.RequestLifeCycleManager;
+
+import org.agilej.sparkle.core.ext.RequestLifeCycleManager;
+import org.agilej.sparkle.mvc.ActionMethod;
 
 public class NettyRequestLifeCycleManager implements RequestLifeCycleManager {
 
--- src/main/java/org/agilej/sparkle/netty4/NettySpecific.java
+++ src/main/java/org/agilej/sparkle/netty4/NettySpecific.java
@@ -1,8 +1,8 @@
 package org.agilej.sparkle.netty4;
 
-import org.agilej.sparkle.core.argument.ArgumentResolverManager;
-import org.agilej.sparkle.core.engine.RequestLifeCycleManager;
+
 import org.agilej.sparkle.core.ext.EnvSpecific;
+import org.agilej.sparkle.core.ext.RequestLifeCycleManager;
 import org.agilej.sparkle.core.ext.VendorArgumentResolverProvider;
 import org.agilej.sparkle.core.ext.VendorViewRenderProvider;
 
